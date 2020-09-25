58bfc219171001eb905dd8193d3b30ca2c76571c
Tapwatero <70451052+Tapwatero@users.noreply.github.com>
2020-09-25 17:12:57 +0100

    Add files via upload

---
+++ src/me/tapwater/Main.java
@@ -0,0 +1,17 @@
+package me.tapwater;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int heightM = 171;
+        int heightF = 172;
+        int combined = heightF + heightM;
+        int divided = combined / 2;
+        int predicted = (int) (divided - 12.7);
+        System.out.println(predicted);
+
+
+
+    }
+}
