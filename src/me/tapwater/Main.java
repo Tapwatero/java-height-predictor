76c592947d7aa1d3b0d8d6ee562f485e2fe68c70
Tapwatero <70451052+Tapwatero@users.noreply.github.com>
2021-05-02 22:56:38 +0100

    Added offset to make prediction more accurate as data is collected.

--- src/me/tapwater/Main.java
+++ src/me/tapwater/Main.java
@@ -8,7 +8,8 @@ public class Main {
         int heightM = 171; // This is your mother's height in centimetres
         int heightF = 172; // This is your father's height in centimetres
         int combined = heightF + heightM; // Combines your mother's and father's height
-        int divided = combined / 2; // Divides combined by 2
+        float offset = 2.24; // This is the offset used to make the predicton more accurate as more people use it.
+        int divided = (combined / 2) + offset; // Divides combined by 2
 
         String gender = "male"; // gender must equal male or female.
         if (gender.equalsIgnoreCase("male")) { // Checks without caring for case if gender is equal to male.
@@ -17,6 +18,7 @@ public class Main {
         } else if (gender.equalsIgnoreCase("female")) { // Checks without caring for case if gender is equal to female.
             int predicted = (int) (divided - 12.7); // The maths required to predict adult height for females.
             System.out.println(predicted); // Prints it out.
+
         }
 
     }
